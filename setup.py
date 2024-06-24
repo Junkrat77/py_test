d8443f51081bec407e01a0a0101862fe4efda65b
@ganquan.gq <ganquan.gq@oceanbase.com>
2024-06-24 17:55:38 +0800

    222

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ganquan',
     version='0.2',
-    long_description="this is a test package",
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
 )
