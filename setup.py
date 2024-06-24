0662c74bf4a9dc115fa2f724b590996023c3872d
@ganquan.gq <ganquan.gq@oceanbase.com>
2024-06-24 19:15:34 +0800

    qqq

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ganquan',
     version='0.2',
+    description="this is a tesk package",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
