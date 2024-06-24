bc37ea1ffb1c515fa8341dd2b8284c1565748fc6
@ganquan.gq <ganquan.gq@oceanbase.com>
2024-06-24 19:22:41 +0800

    111

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ganquan',
     version='0.2',
     description="this is a tesk package",
-    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
     packages=find_packages(),
 )
