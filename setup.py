19eb35049e5610e99197ccf42280551ca0716003
@ganquan.gq <ganquan.gq@oceanbase.com>
2024-06-26 11:27:57 +0800

    11

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ setup(
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
     license='Apache-2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License 2.0',
+    ],
     packages=find_packages(),
 )
